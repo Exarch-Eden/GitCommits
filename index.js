0fd766612d89c93d9e61b728c17ebef665c5571c
Kent Claudio <rikysmile33@gmail.com>
2021-08-04 19:00:51 -0700

    Updated index JS
    Added "/commit" endpoint and rudimentary GitHub API commit fetching

--- index.js
+++ index.js
@@ -12,6 +12,44 @@ app.get("/", async (req, res) => {
   res.status(200).send('Endpoint "/" received your GET request');
 });
 
+app.get("/commit", async (req, res) => {
+  // mandatory query parameters
+  const ownerName = req.query.owner;
+  const repoName = req.query.repo;
+
+  // for testing purposes (remove later)
+  // const ownerName = "Exarch-Eden";
+  // const repoName = "GitCommits";
+
+  // optional query parameters
+  const branch = req.query.branch;
+  const hash = req.query.hash;
+
+  // for testing purposes
+  console.log("\n", { branch, hash }, "\n");
+
+  // github API url
+  const targetFetchUrl = `https://api.github.com/repos/${ownerName}/${repoName}/commits`;
+
+  let data = {};
+
+  try {
+    const fetchedRes = await fetch(targetFetchUrl);
+    data = await fetchedRes.json();
+    console.log("\n", data, "\n");
+  } catch (error) {
+    console.log(
+      "--------------ERROR--------------",
+      error,
+      "---------------------------------"
+    );
+    res.status(500).send();
+    return;
+  }
+
+  res.status(200).send(JSON.stringify(data));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });

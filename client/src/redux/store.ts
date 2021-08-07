b47dd521b78147b75c5737e63c77a3fae32a3a5a
Kent Claudio <rikysmile33@gmail.com>
2021-08-07 16:24:35 -0700

    Created linkInput slice reducer
    Updated store TS file with linkInput slice reducer

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import linkInputReducer from './reducers/linkInputSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    link: linkInputReducer
   },
 });
 

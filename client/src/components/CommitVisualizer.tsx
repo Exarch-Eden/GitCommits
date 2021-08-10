9bc99d2320a3bf5a667348faaacef0c7139b44db
Kent Claudio <rikysmile33@gmail.com>
2021-08-09 19:07:41 -0700

    Updated CommitVisualizer component
    Created function to map and render the retrieved commit data into readable format

--- client/src/components/CommitVisualizer.tsx
+++ client/src/components/CommitVisualizer.tsx
@@ -1,16 +1,56 @@
-import React, { FC } from "react";
+import React, { FC, ReactElement } from "react";
 import { useAppSelector } from "../redux/hooks";
 import { selectCommitData } from "../redux/reducers/commitDataSlice";
+import { CommitArray, CommitInfo, SingleCommit } from "../types";
+import SingleCommitVisualizer from "./SingleCommitVisualizer";
+
+import "../styles/CommitVisualizer.css"
 
 export interface CommitVisualizerProps {}
 
+/**
+ * Component responsible for visualizing the raw fetched data into
+ * readable format for the user.
+ *
+ * @returns
+ */
 const CommitVisualizer: FC<CommitVisualizerProps> = () => {
   const commitData = useAppSelector(selectCommitData);
 
   return (
     <div className="commitVisualizerContainer">
-      {JSON.stringify(commitData)}
+      {/* {JSON.stringify(commitData)} */}
+      {renderCommitInfo(commitData)}
     </div>
+  );
+};
+
+/**
+ * Renders the raw fetched data passed in into readable format.
+ *
+ * @param rawCommitArray Raw fetched data to format.
+ * @returns Component list of each commit in readable format.
+ */
+const renderCommitInfo = (rawCommitArray: CommitArray): ReactElement => {
+  return (
+    <>
+      {rawCommitArray.map((curCommit: SingleCommit, index: number) => {
+
+        const commitInfo: CommitInfo = {
+          message: curCommit.commit?.message,
+          author: {
+            realName: curCommit.commit?.author?.name,
+            userName: curCommit.author?.login,
+          },
+        };
+
+        return (
+          <div key={index}>
+            <SingleCommitVisualizer commitInfo={commitInfo} />
+          </div>
+        );
+      })}
+    </>
   );
 };
 

bf4bb117a6431cde2f8ef5fdb914320a23738099
Kent Claudio <rikysmile33@gmail.com>
2021-08-12 16:11:03 -0700

    Updated FileSelector component
    Moved renderFileList() function in FileSelector component from outside the FC to within
    
    FileSelector component now highlights the selected file
    
    Refactored fileChanges slice reducer setCurrent() identifier to "setCurrentFile"
    
    Updated styling of FileSelector component

--- client/src/components/FileSelector.tsx
+++ client/src/components/FileSelector.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from "react";
 import { ReactElement } from "react";
-import { useAppSelector } from "../redux/hooks";
-import { selectFileList } from "../redux/reducers/fileChangesSlice";
+import { useCallback } from "react";
 
+// redux imports
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import {
+  selectCurrentFile,
+  selectFileList,
+  setCurrentFile,
+} from "../redux/reducers/fileChangesSlice";
+
+// type imports
 import { File } from "../types";
 
 import "../styles/FileSelector.css";
@@ -10,23 +18,39 @@ import "../styles/FileSelector.css";
 interface FileSelectorProps {}
 
 const FileSelector: FC<FileSelectorProps> = () => {
+  const currentFile = useAppSelector(selectCurrentFile);
   const fileList = useAppSelector(selectFileList);
+
+  const dispatch = useAppDispatch();
+
+  // const renderFileList = (fileList: File[], fileOnClick: (fileName: string) => void): ReactElement => {
+  const renderFileList = (fileList: File[]): ReactElement => {
+    return (
+      <ul className="fileList">
+        {fileList.map((curFile: File, index: number) => {
+          // class name assigned to the currently selected file
+          const isActiveClassName =
+            curFile.filename === currentFile ? "activeFile" : "";
+          
+          return (
+            <li
+              key={index}
+              onClick={() => dispatch(setCurrentFile(curFile.filename!))}
+              className={isActiveClassName}
+            >
+              {curFile.filename}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
 
   return (
     <div className="fileSelectorContainer">
-      <p>This is the file selector.</p>
+      {/* <p>This is the file selector.</p> */}
       {renderFileList(fileList)}
     </div>
-  );
-};
-
-const renderFileList = (fileList: File[]): ReactElement => {
-  return (
-    <ul className="fileList">
-      {fileList.map((curFile: File, index: number) => {
-        return <li key={index}>{curFile.filename}</li>;
-      })}
-    </ul>
   );
 };
 
--- client/src/redux/reducers/fileChangesSlice.ts
+++ client/src/redux/reducers/fileChangesSlice.ts
@@ -16,7 +16,7 @@ export const fileChangesSlice = createSlice({
   name: "file",
   initialState,
   reducers: {
-    setCurrent: (state, action: PayloadAction<string>) => {
+    setCurrentFile: (state, action: PayloadAction<string>) => {
       state.current = action.payload;
     },
     setFileList: (state, action: PayloadAction<File[]>) => {
@@ -25,7 +25,7 @@ export const fileChangesSlice = createSlice({
   },
 });
 
-export const { setCurrent, setFileList } = fileChangesSlice.actions;
+export const { setCurrentFile, setFileList } = fileChangesSlice.actions;
 
 export const selectCurrentFile = (state: RootState) => state.file.current;
 export const selectFileList = (state: RootState) => state.file.fileList;

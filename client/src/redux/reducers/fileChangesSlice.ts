7d26ba912bd0d21809c4f0ee6c7cf1e965db9d89
Kent Claudio <rikysmile33@gmail.com>
2021-08-12 03:45:34 -0700

    Created fileChange slice reducer
    Implemented fileChange slice reducer to store TS file

---
+++ client/src/redux/reducers/fileChangesSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { File } from "../../types";
+import { RootState } from "../store";
+
+export interface FileChangesState {
+  current: string;
+  fileList: File[];
+}
+
+const initialState: FileChangesState = {
+  current: "",
+  fileList: [],
+};
+
+export const fileChangesSlice = createSlice({
+  name: "file",
+  initialState,
+  reducers: {
+    setCurrent: (state, action: PayloadAction<string>) => {
+      state.current = action.payload;
+    },
+    setFileList: (state, action: PayloadAction<File[]>) => {
+      state.fileList = action.payload;
+    },
+  },
+});
+
+export const { setCurrent, setFileList } = fileChangesSlice.actions;
+
+export const selectCurrentFile = (state: RootState) => state.file.current;
+export const selectFileList = (state: RootState) => state.file.fileList;
+
+export default fileChangesSlice.reducer;
